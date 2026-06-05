ae01c6722581e9d53b46ed680d7cc736bdb95f66
paoloanzn <paolo@gladium.ai>
2026-06-05 12:01:34 +0200

    (docs) add README.md and update file's license headers

---
+++ README.md
@@ -0,0 +1,4 @@
+fcf
+---
+
+CLI tooling and EVM Smart contracts for [@freecodexyz](https://freecodefund.xyz) funding layer.
--- contracts/test/RIK.t.sol
+++ contracts/test/RIK.t.sol
@@ -1,5 +1,5 @@
 // test/RIK_1.t.sol
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0 
 pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
