0e93e217443381c3c7a77b3a97edd1f1a2f60133
paoloanzn <paolo@gladium.ai>
2026-07-15 19:39:42 +0200

    (feat) implement OS detection and support validation functions

---
+++ packages/vfnode/src/os.ts
@@ -0,0 +1,18 @@
+import { type } from "node:os";
+import { type Result, ok, err, assertNever } from "./result.js";
+
+export type OperatingSystem =  "Linux" | "Darwin" | "Windows_NT";
+
+export function getOs(): OperatingSystem {
+    return type() as OperatingSystem;
+}
+
+export function isOsSupported(os: OperatingSystem): Result<Boolean, "unsupported_os"> {
+    switch (os) {
+        case "Linux": return ok(true);
+        case "Darwin": return ok(true);
+        case "Windows_NT": return err("unsupported_os");
+
+        default: return assertNever(os);
+    }
+}
