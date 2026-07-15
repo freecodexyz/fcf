53a319463398cebc71efa0e3c3a32e48b1df7ddd
paoloanzn <paolo@gladium.ai>
2026-07-15 19:23:22 +0200

    (feat) add assertNever function

--- packages/vfnode/src/result.ts
+++ packages/vfnode/src/result.ts
@@ -4,3 +4,8 @@ export type Result<T, E = Error> =
 
 export const ok = <T>(value: T): Result<T, never> => ({ ok: true, value });
 export const err = <E>(error: E): Result<never, E> => ({ ok: false, error });
+
+export function assertNever(x: never): never {
+  throw new Error(`Unhandled: ${JSON.stringify(x)}`);
+}
+
